733537a6cbb865bdc855c89732bf837bbd9e9e4b
Pmaaagnu <79535753+Mr-Sniff@users.noreply.github.com>
2026-03-18 13:36:51 +0100

    foto sida

--- generate_html.rb
+++ generate_html.rb
@@ -2,7 +2,7 @@ def generate_pics(files)
   i = 0
   while i < files.size()
     if File.directory?(files[i])
-      puts "<h2>" + String(files[i]) + "<h2>"
+      puts "<h2 class = \"portfolio_photo_title\">" + String(files[i]) + "</h2>"
       generate_pics(Dir.glob(files[i] + "/*"))
     else
       generate_image_tag(files[i])
@@ -12,7 +12,7 @@ def generate_pics(files)
 end
 
 def generate_image_tag(file)
-  puts "<img src = \"" + file + "\" alt = \"photo\"> class = \"\""
+  puts "<img src = \"" + file + "\" alt = \"photo\" class = \"portfolio_photo\">"
 end
 
 dir = "img/photo_portfolio"
