eecc44119553826e19cb2383ced5b827ae354eb0
Mr-Sniff <teodboe@gmail.com>
2026-03-18 16:58:55 +0100

    more images, zoom in fixed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 let menuToggle = document.querySelector("#menu-toggle")
 let navPane = document.querySelector("#nav-pane")
 let mobileNav = document.querySelector("#mobile-nav")
-let portfolioPhoto = document.querySelector('.portfolio_photo')
+let portfolioPhoto = document.querySelectorAll('.portfolio_photo')
 
 menuToggle.addEventListener('click', show)
-portfolioPhoto.addEventListener('click', fullscreen_photo)
+portfolioPhoto.forEach(img => {
+  img.addEventListener('click', fullscreen_photo)
+});
 
 function show() {
   navPane.classList.toggle('active')
 };
 
 function fullscreen_photo() {
-  portfolioPhoto.classList.toggle('fullscreen')
-}
+  this.classList.toggle('fullscreen')
+};
