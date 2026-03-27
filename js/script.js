b8f6610e67793feb7076c3c5a6d20f1f4129cb18
Pmaaagnu <79535753+Mr-Sniff@users.noreply.github.com>
2026-03-27 14:22:12 +0100

    fick scroll knappen att funka nu också!

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let menuToggle = document.querySelector("#menu-toggle")
 let navPane = document.querySelector("#nav-pane")
 let mobileNav = document.querySelector("#mobile-nav")
 let portfolioPhoto = document.querySelectorAll('.portfolio_photo')
+let topButton = document.getElementById("back-to-top-button");
+
 
 menuToggle.addEventListener('click', show)
 portfolioPhoto.forEach(img => {
@@ -12,6 +14,22 @@ function show() {
   navPane.classList.toggle('active')
 };
 
+window.addEventListener('scroll', show_button)
+function show_button() {
+  if (window.scrollY > 250) {
+    topButton.classList.add('show')
+  } else {
+    topButton.classList.remove('show')
+  }
+};
+
 function fullscreen_photo() {
   this.classList.toggle('fullscreen')
 };
+
+//tagen från w3schools.com
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
